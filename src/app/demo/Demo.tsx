8cf4ad87d04767caaa5cd417655491a303ecd3d9
Francesco Galisi <galisifrancesco@gmail.com>
2024-02-10 10:10:13 +0100

    Add faker js

--- src/app/demo/Demo.tsx
+++ src/app/demo/Demo.tsx
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import { useSearchSetContext } from "../App";
 
 export default function Demo() {
@@ -7,11 +8,11 @@ export default function Demo() {
     <div className="p-4">
       <button
         className="h-10 min-w-20 rounded bg-gray-300 px-2 text-gray-700 hover:bg-gray-200 hover:text-gray-600 active:bg-gray-100 active:text-gray-500"
-        onClick={() => {
-          searchSet.ingest({
+        onClick={async () => {
+          void searchSet.ingest({
             id: window.crypto.randomUUID(),
-            address: "Via dalle palle",
-            name: "Gigio",
+            address: faker.location.streetAddress(),
+            name: faker.person.fullName(),
             phoneNumber: "+66 666 666666",
           });
         }}
