af4df58b031eedba64e282f5f531e640cae12fd2
Francesco Galisi <galisifrancesco@gmail.com>
2024-02-10 21:21:38 +0100

    Style disabled button

--- src/common/components/Button.tsx
+++ src/common/components/Button.tsx
@@ -7,7 +7,7 @@ export default function Button({ className, ...props }: ButtonProps) {
   return (
     <button
       className={twMerge(
-        "h-10 min-w-20 rounded bg-gray-300 px-2 text-gray-700 hover:bg-gray-200 hover:text-gray-600 active:bg-gray-100 active:text-gray-500",
+        "h-10 min-w-20 rounded bg-gray-300 px-2 text-gray-700 enabled:hover:bg-gray-200 enabled:hover:text-gray-600 enabled:active:bg-gray-100 enabled:active:text-gray-500 disabled:opacity-25",
         className,
       )}
       {...props}
