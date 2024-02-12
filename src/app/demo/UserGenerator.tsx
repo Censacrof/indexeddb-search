45f6c10b6f42798aa7b3859e333ea088f04aa1cd
Francesco Galisi <galisifrancesco@gmail.com>
2024-02-12 17:05:52 +0100

    Show how long it took to ingest

--- src/app/demo/UserGenerator.tsx
+++ src/app/demo/UserGenerator.tsx
@@ -1,4 +1,5 @@
 import { faker } from "@faker-js/faker";
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import Button from "../../common/components/Button";
 import Pre from "../../common/components/Pre";
@@ -6,31 +7,30 @@ import { User } from "../../features/indexeddbSearch/indexeddbSearch";
 import { useSearchSetContext } from "../App";
 
 export default function UserGenerator() {
-  const { searchSet } = useSearchSetContext();
+  const {
+    mutateAsync: ingestUsers,
+    data: ingestUsersData,
+    isPending: isIngestPending,
+    isSuccess: isIngestSuccess,
+  } = useIngestUsers();
+
   const [userToIngest, setUserToIngest] = useState(randomUser());
-  const [isIngestDisabled, setIsIngestDisabled] = useState(false);
 
   return (
     <div className="flex gap-2">
       <div className="flex shrink-0 flex-col gap-2">
         <Button
           onClick={async () => {
-            setIsIngestDisabled(true);
-
-            await searchSet.ingest(userToIngest);
+            await ingestUsers([userToIngest]);
             setUserToIngest(randomUser());
-
-            setIsIngestDisabled(false);
           }}
-          disabled={isIngestDisabled}
+          disabled={isIngestPending}
         >
           Ingest user
         </Button>
 
         <Button
           onClick={async () => {
-            setIsIngestDisabled(true);
-
             let uToIng = userToIngest;
             const allUsersToIngest = Array.from(new Array(10000).keys()).map(
               () => {
@@ -40,16 +40,17 @@ export default function UserGenerator() {
               },
             );
 
-            await searchSet.ingest(...allUsersToIngest);
+            await ingestUsers(allUsersToIngest);
 
             setUserToIngest(uToIng);
-
-            setIsIngestDisabled(false);
           }}
-          disabled={isIngestDisabled}
+          disabled={isIngestPending}
         >
           Ingest user (10000x)
         </Button>
+
+        {isIngestPending && <span>Ingesting...</span>}
+        {isIngestSuccess && <span>Done in {ingestUsersData.tookMs}ms</span>}
       </div>
       <Pre>{JSON.stringify(userToIngest, undefined, 2)}</Pre>
     </div>
@@ -64,4 +65,16 @@ function randomUser(): User {
     phoneNumber: faker.phone.number(),
     note: faker.lorem.paragraphs(),
   };
+}
+
+function useIngestUsers() {
+  const { searchSet } = useSearchSetContext();
+
+  return useMutation({
+    mutationFn: async (usersToIngest: User[]) => {
+      const startTime = Date.now();
+      await searchSet.ingest(...usersToIngest);
+      return { tookMs: Date.now() - startTime };
+    },
+  });
 }
